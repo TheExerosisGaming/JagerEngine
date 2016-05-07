2a6e5a5777dc704c06780fa8a2c10b5d1cfae524
LakkieGaming <lakkiegaming@gmail.com>
2016-05-07 12:11:19 -0400

    Support for multiple players in 1 instance

--- src/main/java/me/exerosis/jager/engine/implementation/components/player/player/BloodComponent.java
+++ src/main/java/me/exerosis/jager/engine/implementation/components/player/player/BloodComponent.java
@@ -1,6 +1,12 @@
 package me.exerosis.jager.engine.implementation.components.player.player;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.function.Predicate;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -10,58 +16,107 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.util.Vector;
 
-import me.exerosis.jager.engine.core.component.Component;
+import me.exerosis.jager.engine.implementation.components.player.PlayerComponent;
 
-public class BloodComponent extends Component {
+public class BloodComponent extends PlayerComponent {
 
-	private Player player = null;
-	private Item[] bloodItems = null;
+	private Map<Player, Predicate<Player>> predicates = new HashMap<Player, Predicate<Player>>();
+	private List<Player> players = new ArrayList<Player>();
+	private Map<Player, Item[]> bloodItems = new LinkedHashMap<Player, Item[]>();
 	private Plugin plugin;
-	
-	public BloodComponent(Plugin plugin, Player player, boolean enabledByDefault) {
-		this.setPlayer(player);
+	private boolean enabledByDefault;
+
+	public BloodComponent(Plugin plugin, boolean enabledByDefault) {
 		this.plugin = plugin;
 		if (enabledByDefault)
 			enable();
-	}
-	
-	public Player getPlayer() {
-		return player;
+		this.enabledByDefault = enabledByDefault;
 	}
 
-	public void setPlayer(Player player) {
-		this.player = player;
+	public void addPlayer(Player p) {
+		players.add(p);
+		predicates.put(p, player -> enabledByDefault);
+	}
+
+	public void addPlayer(Player p, boolean enabled) {
+		players.add(p);
+		predicates.put(p, player -> enabled);
+	}
+
+	public void setEnabledPlayer(Player p, boolean enabled) {
+		predicates.put(p, player -> enabled);
+		
+	}
+
+	public void removePlayer(Player p) {
+		players.remove(p);
+		predicates.remove(p);
+	}
+
+	public void removePlayer(int index) {
+		predicates.remove(players.get(index));
+		players.remove(index);
+	}
+
+	public Map<Player, Predicate<Player>> getPredicates() {
+		return predicates;
+	}
+
+	public List<Player> getPlayers() {
+		return players;
 	}
 
 	@SuppressWarnings("deprecation")
 	public void onEnable() {
-		bloodItems = new Item[4];
-		for (int i = 0; i < 4; i++) {
-			Item itemEntity = player.getWorld().dropItem(player.getLocation(), new ItemStack(Material.getMaterial(351)));
-			ItemStack item = new ItemStack(Material.getMaterial(351));
-			item.setDurability((short) 1);
-			itemEntity.setItemStack(item);
-			itemEntity.setPickupDelay(1000);
-			Random r = new Random();
-			itemEntity.setVelocity(new Vector().setX(r.nextInt(4) - 2).setZ(r.nextInt(4) - 2).setY(0));
-			itemEntity.setCustomName("§cBlood");
-			itemEntity.setCustomNameVisible(true);
-			bloodItems[i] = itemEntity;
+		for (Player player : getPlayers()) {
+			Item[] items = new Item[4];
+			for (int i = 0; i < 4; i++) {
+				Item itemEntity = player.getWorld().dropItem(player.getLocation(), new ItemStack(Material.getMaterial(351)));
+				ItemStack item = new ItemStack(Material.getMaterial(351));
+				item.setDurability((short) 1);
+				itemEntity.setItemStack(item);
+				itemEntity.setPickupDelay(1000);
+				Vector v = new Vector();
+				v.setX(nextFloat(new Random(), 0.4f));
+				v.setZ(nextFloat(new Random(), 0.4f));
+				v.setY(nextFloat(new Random(), 0.5f));
+				itemEntity.setVelocity(v);
+				itemEntity.setCustomName("§cBlood");
+				itemEntity.setCustomNameVisible(true);
+				items[i] = itemEntity;
+			}
+			bloodItems.put(player, items);
 		}
 		Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
 			public void run() {
-				for (int i = 0; i < 4; i++) {
-					bloodItems[i].remove();
+				for (Player player : getPlayers()) {
+					for (int i = 0; i < 4; i++) {
+						if (bloodItems.get(player) == null) {
+							Bukkit.getLogger().info("Player is equal to null!");
+							break;
+						}
+						bloodItems.get(player)[i].remove();
+					}
 				}
 			}
 		}, 15l);
 	}
 
+	public float nextFloat(Random r, float maxValue) {
+		while (true) {
+			float f = r.nextFloat();
+			if (f > maxValue)
+				continue;
+			else
+				return f;
+		}
+	}
+	
 	public void onDisable() {
-		if (bloodItems == null)
-			return;
-		for (Item i : bloodItems) {
-			i.remove();
+		for (Player player : getPlayers()) {
+			for (int i = 0; i < 4; i++) {
+				bloodItems.get(player)[i].remove();
+			}
 		}
 	}
 
